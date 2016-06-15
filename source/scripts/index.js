f7915a055d748538f7b226ca2257878e7cb54218
Kory Nunn <knunn187@gmail.com>
2016-06-15 13:08:06 +1000

    demauriced

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,28 +1,20 @@
 var cpjax = require('cpjax'),
     crel = require('crel'),
-    australiasLeadingJavascriptDeveloper = window.location.hash.slice(1).toLowerCase() || 'mauricebutler';
+    shuffle = require('shuffle-array'),
+    australiasLeadingJavascriptDeveloperName = window.location.hash.slice(1).toLowerCase();
 
 var membersUrl = 'https://api.github.com/orgs/australiasleadingjavascriptdeveloper/public_members';
 
 function renderMembers(members){
     var membersElement = document.querySelector('.members');
 
-    members.sort(function(a, b){
-        if(a.login.toLowerCase() === australiasLeadingJavascriptDeveloper){
-            return -1;
-        }
-
-        if(b.login.toLowerCase() === australiasLeadingJavascriptDeveloper){
-            return 1;
-        }
-
-        return 0;
-    });
+    var randomizedMembers = shuffle(members, {copy: true});
 
     crel(membersElement,
         crel('p', 'Made by this developer'),
-        members.map(function(member){
-            return crel('a', {href: member.html_url, class:'member'},
+        randomizedMembers.map(function(member){
+            var isAustraliasLeadingJavascriptDeveloper = member.login.toLowerCase() === australiasLeadingJavascriptDeveloperName;
+            return crel('a', {href: member.html_url, class:'member' + (isAustraliasLeadingJavascriptDeveloper ? ' leading' : '')},
                 crel('img', {src: member.avatar_url}),
                 crel('h3', member.login)
             );
